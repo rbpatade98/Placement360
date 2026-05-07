09f781957b68cd0c394817e9607663bddf3c3b72
Ramchandra Patade <rbpatade98@gmail.com>
2026-05-07 12:54:58 +0530

    First commit

---
+++ placement360/src/App.tsx
@@ -0,0 +1,10 @@
+
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div className="text-blue-600">
+      <Button variant="destructive">click me</Button>  
+    </div>
+  )
+}
